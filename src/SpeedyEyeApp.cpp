e0e72af97068d27c401309b81120c104d3ede745
Micah Elizabeth Scott <micah@misc.name>
2015-02-20 11:38:17 -0800

    Cinder project build on Mac

--- src/SpeedyEyeApp.cpp
+++ src/SpeedyEyeApp.cpp
@@ -4,8 +4,6 @@
 #include "cinder/Utilities.h"
 #include "cinder/Thread.h"
 
-#include "ciUI.h"
-
 #include "ps3eye.h"
 
 using namespace ci;
@@ -60,23 +58,6 @@ static void yuv422_to_rgba(const uint8_t *yuv_src, const int stride, uint8_t *ds
     }
 }
 
-class eyeFPS : public ciUIFPS
-{
-public:
-    eyeFPS(float x, float y, int _size):ciUIFPS(x,y,_size)
-    {
-    }
-    
-    eyeFPS(int _size):ciUIFPS(_size)
-    {
-    }
-    
-	void update_fps(float fps)
-	{
-		setLabel("FPS: " + numToString(fps, labelPrecision));
-	}
-};
-
 class PS3EYECaptureApp : public AppBasic {
   public:
 	void setup();
@@ -86,10 +67,6 @@ class PS3EYECaptureApp : public AppBasic {
 	void shutdown();
 
 	void eyeUpdateThreadFn();
-
-	ciUICanvas *gui;
-    eyeFPS *eyeFpsLab;
-    void guiEvent(ciUIEvent *event);
 
     ps3eye::PS3EYECam::PS3EYERef eye;
 
@@ -124,8 +101,6 @@ void PS3EYECaptureApp::setup()
 	mCamFpsLastSampleFrame = 0;
 	mCamFpsLastSampleTime = 0;
 
-	gui = new ciUICanvas(0,0,320, 480);
-    
     float gh = 15;
     float slw = 320 - 20;
 
@@ -143,80 +118,6 @@ void PS3EYECaptureApp::setup()
 		
 		// create and launch the thread
 		mThread = thread( bind( &PS3EYECaptureApp::eyeUpdateThreadFn, this ) );
-        
-        gui->addWidgetDown(new ciUILabel("EYE", CI_UI_FONT_MEDIUM));
-        
-        eyeFpsLab = new eyeFPS(CI_UI_FONT_MEDIUM);
-        gui->addWidgetRight(eyeFpsLab);
-        
-        // controls
-        gui->addWidgetDown(new ciUIToggle(gh, gh, false, "auto gain"));
-        gui->addWidgetRight(new ciUIToggle(gh, gh, false, "auto white balance"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 63, eye->getGain(), "gain"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 63, eye->getSharpness(), "sharpness"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 255, eye->getExposure(), "exposure"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 255, eye->getBrightness(), "brightness"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 255, eye->getContrast(), "contrast"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 255, eye->getHue(), "hue"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 255, eye->getBlueBalance(), "blue balance"));
-        gui->addWidgetDown(new ciUISlider(slw, gh, 0, 255, eye->getRedBalance(), "red balance"));
-        
-        gui->registerUIEvents(this, &PS3EYECaptureApp::guiEvent);
-    }
-}
-
-void PS3EYECaptureApp::guiEvent(ciUIEvent *event)
-{
-    string name = event->widget->getName();
-    if(name == "auto gain")
-    {
-        ciUIToggle *t = (ciUIToggle * ) event->widget;
-        eye->setAutogain(t->getValue());
-    }
-    else if(name == "auto white balance")
-    {
-        ciUIToggle *t = (ciUIToggle * ) event->widget;
-        eye->setAutoWhiteBalance(t->getValue());
-    }
-    else if(name == "gain")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setGain(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "sharpness")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setSharpness(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "exposure")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setExposure(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "brightness")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setBrightness(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "contrast")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setContrast(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "hue")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setHue(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "blue balance")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setBlueBalance(static_cast<uint8_t>(s->getScaledValue()));
-    }
-    else if(name == "red balance")
-    {
-        ciUISlider *s = (ciUISlider *) event->widget;
-        eye->setRedBalance(static_cast<uint8_t>(s->getScaledValue()));
     }
 }
 
@@ -238,7 +139,6 @@ void PS3EYECaptureApp::shutdown()
     eye->stop();
     //
 	delete[] frame_bgra;
-	delete gui;
 }
 
 void PS3EYECaptureApp::mouseDown( MouseEvent event )
@@ -265,9 +165,6 @@ void PS3EYECaptureApp::update()
             mCamFpsLastSampleTime = now;
             mCamFpsLastSampleFrame = mCamFrameCount;
         }
-    
-        gui->update();
-        eyeFpsLab->update_fps(mCamFps);
     }
 }
 
@@ -284,8 +181,6 @@ void PS3EYECaptureApp::draw()
 		gl::draw( mTexture );
 		glPopMatrix();
 	}
-	
-	gui->draw();
 }
 
 CINDER_APP_BASIC( PS3EYECaptureApp, RendererGl )
